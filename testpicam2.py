70e5b0cca32002c6e67d1b20777660f041c340d9
odin5on <108775964+odin5on@users.noreply.github.com>
2024-02-14 09:50:55 -0600

    try rotating

--- testpicam2.py
+++ testpicam2.py
@@ -1,16 +1,22 @@
+import numpy as np
+from picamera2 import Picamera2, H264Encoder
 import time
 
-from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+class RotatingEncoder(H264Encoder):
+    def input(self, frame):
+        # Rotate the frame by 180 degrees
+        frame = np.rot90(frame, 2)
+        super().input(frame)
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
-encoder = H264Encoder(10000000)
-
+encoder = RotatingEncoder(10000000)
 picam2.start_recording(encoder, 'test.h264')
+
 time.sleep(5)
+
 picam2.stop_recording()
 
 print('done')

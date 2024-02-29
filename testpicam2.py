9dbe442a34108c66acf9e6f4692104bc71d7634b
odin5on <bodin.danny@gmail.com>
2024-02-29 14:01:47 -0600

    remove rotation in script

--- testpicam2.py
+++ testpicam2.py
@@ -6,7 +6,6 @@ import libcamera
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
-video_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
 picam2.configure(video_config)
 
 print(picam2.camera_controls)

c0a8b8d306119737914de2e0acb390631712f384
odin5on <108775964+odin5on@users.noreply.github.com>
2024-03-10 15:14:28 -0500

    remove lo-res formats

--- run-pi-webcam.sh
+++ run-pi-webcam.sh
@@ -44,35 +44,35 @@ create_uvc() {
 	echo "	Creating UVC gadget functionality : $FUNCTION"
 	mkdir functions/$FUNCTION
 
-	create_frame $FUNCTION 640 480 uncompressed u "333333
-416667
-500000
-666666
-1000000
-1333333
-2000000
-"
-	create_frame $FUNCTION 1280 720 uncompressed u "1000000
-1333333
-2000000
-"
+# 	create_frame $FUNCTION 640 480 uncompressed u "333333
+# 416667
+# 500000
+# 666666
+# 1000000
+# 1333333
+# 2000000
+# "
+# 	create_frame $FUNCTION 1280 720 uncompressed u "1000000
+# 1333333
+# 2000000
+# "
 	create_frame $FUNCTION 1920 1080 uncompressed u "2000000"
-	create_frame $FUNCTION 640 480 mjpeg m "333333
-416667
-500000
-666666
-1000000
-1333333
-2000000
-"
-	create_frame $FUNCTION 1280 720 mjpeg m "333333
-416667
-500000
-666666
-1000000
-1333333
-2000000
-"
+# 	create_frame $FUNCTION 640 480 mjpeg m "333333
+# 416667
+# 500000
+# 666666
+# 1000000
+# 1333333
+# 2000000
+# "
+# 	create_frame $FUNCTION 1280 720 mjpeg m "333333
+# 416667
+# 500000
+# 666666
+# 1000000
+# 1333333
+# 2000000
+# "
 	create_frame $FUNCTION 1920 1080 mjpeg m "333333
 416667
 500000
